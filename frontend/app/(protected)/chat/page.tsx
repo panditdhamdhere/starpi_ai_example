990f552bfb1751dc8de9f87936785d619a033465
panditdhamdhere <dhamdherepandit@gmail.com>
2026-05-23 22:03:06 +0530

    code sec add

--- frontend/app/(protected)/chat/page.tsx
+++ frontend/app/(protected)/chat/page.tsx
@@ -1,25 +1,25 @@
-import WorkspaceAiChat from '@/components/workspace/workspace-ai-chat'
-import { MessageCircle } from 'lucide-react'
-import React from 'react'
+import WorkspaceAiChat from "@/components/workspace/workspace-ai-chat";
+import { MessageCircle } from "lucide-react";
+import React from "react";
 
 const page = () => {
-  return <WorkspaceAiChat
+  return (
+    <WorkspaceAiChat
+      mode="chat"
+      title="Chat"
+      badge="Powered by Gemini 2.5 flash"
+      placeholder="Send a mesaage..."
+      emptyState={{
+        icon: (
+          <span className="flex size-12 items-center justify-center rounded-full bg-primary/10 text-primary">
+            <MessageCircle className="size-6" />
+          </span>
+        ),
+        title: "start a conversation",
+        description: "Ask anythig -- answer stream in as the model thinks",
+      }}
+    />
+  );
+};
 
-    mode="chat"
-    title="Chat"
-    badge="Powered by Gemini 2.5 flash"
-    placeholder="Send a mesaage..."
-    emptyState={{
-      icon: (
-        <span className='flex size-12 items-center justify-center rounded-full bg-primary/10 text-primary'>
-          <MessageCircle className="size-6" />
-        </span>
-      ),
-      title: "start a conversation",
-      description: "Ask anythig -- answer stream in as the model thinks",
-    }}
-  />
-
-}
-
-export default page 
+export default page;
--- frontend/app/(protected)/code/page.tsx
+++ frontend/app/(protected)/code/page.tsx
@@ -1,8 +1,24 @@
+import WorkspaceAiChat from '@/components/workspace/workspace-ai-chat'
+import { Code } from 'lucide-react'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <WorkspaceAiChat
+      mode="code"
+      title="Code"
+      badge="Code Assistant Gemini 2.5 flash"
+      placeholder="Describe your coding problem or ask for code suggestions"
+      emptyState={{
+        icon: (
+          <span className="flex size-12 items-center justify-center rounded-full bg-primary/10 text-primary">
+            <Code className="size-6" />
+          </span>
+        ),
+        title: "Pair on code with AI",
+        description: "Get Refactor, reviews and debugging help",
+      }}
+    />
   )
 }
 
